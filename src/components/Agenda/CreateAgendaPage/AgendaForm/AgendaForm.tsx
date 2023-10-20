9580741a1a4e78afa575ae3619c8f3681d821e1f
roksana-rachel <roksana.rachel@gmail.com>
2023-10-20 14:20:19 +0200

    added create new agenda template

---
+++ src/components/Agenda/CreateAgendaPage/AgendaForm/AgendaForm.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const AgendaForm = () => <div>Agenda form</div>;
+
+export default AgendaForm;
--- src/components/Agenda/CreateAgendaPage/CreateAgendaPage.tsx
+++ src/components/Agenda/CreateAgendaPage/CreateAgendaPage.tsx
@@ -1,5 +1,48 @@
 import React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
+import { useNavigate } from 'react-router-dom';
+import routes from '@components/App/routing/routes';
+import Typography from '@mui/material/Typography';
+import AgendaNavigation from './AgendaNavigation/AgendaNavigation';
+import AgendaForm from './AgendaForm/AgendaForm';
 
-const CreateAgendaPage = () => <div>Create Agenda Page</div>;
+const CreateAgendaPage = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <Button
+        variant="text"
+        startIcon={<KeyboardArrowLeftIcon />}
+        sx={{ padding: '0.625rem 1rem' }}
+        onClick={() => navigate(routes.homepage)}
+      >
+        Home
+      </Button>
+      <Box
+        display="flex"
+        flexDirection="row"
+        alignItems="center"
+        justifyContent="space-between"
+        sx={{ padding: '0.5rem 0 2.375rem' }}
+      >
+        <Typography variant="h2">Create new agenda</Typography>
+        <Box display="flex" sx={{ gap: '0.75rem' }}>
+          <Button variant="text" sx={{ padding: '0.75rem' }}>
+            Delete
+          </Button>
+          <Button variant="outlined">Publish</Button>
+          <Button>Save as draft</Button>
+        </Box>
+      </Box>
+      <Box display="flex" sx={{ gap: '6rem' }}>
+        <AgendaNavigation />
+        <AgendaForm />
+      </Box>
+    </div>
+  );
+};
 
 export default CreateAgendaPage;
