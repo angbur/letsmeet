197d3929dd659a79ed05a7297ffd5d5424ce1869
roksana-rachel <roksana.rachel@gmail.com>
2023-10-21 21:36:49 +0200

    added theme card and theme page

---
+++ src/components/Agenda/CreateAgendaPage/AgendaForm/components/AgendaTheme/components/ThemeCard/ThemeCard.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { styled } from '@mui/material';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardMedia';
+import CardMedia from '@mui/material/CardMedia';
+import CardActions from '@mui/material/CardActions';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+
+const StyledCard = styled(Card)(({ theme }) => ({
+  width: '22.5rem',
+  boxShadow: 'none',
+  border: '1px solid #CAC4D0',
+  borderRadius: '12px',
+  background: theme.palette.secondary.light,
+}));
+
+type ThemeCardProps = {
+  themeName: string;
+  themeImage: string;
+  applied: boolean;
+  // eslint-disable-next-line no-unused-vars
+  handleAppliedTheme: (newTheme: string) => void;
+};
+
+const ThemeCard = ({ themeName, themeImage, applied, handleAppliedTheme }: ThemeCardProps) => {
+  return (
+    <StyledCard>
+      <CardHeader sx={{ padding: '1rem' }}>
+        <Typography variant="h4" sx={{ fontWeight: 500 }}>
+          {themeName.charAt(0).toUpperCase() + themeName.slice(1)}
+        </Typography>
+      </CardHeader>
+      <CardMedia sx={{ height: '125px' }} image={`/assets/images/themes/${themeImage}`} title={themeName}></CardMedia>
+      <CardActions sx={{ padding: '1rem', justifyContent: 'flex-end' }}>
+        <Button variant="outlined">Preview</Button>
+        <Button disabled={applied} onClick={() => handleAppliedTheme(themeName)}>
+          {applied ? 'Applied' : 'Use'}
+        </Button>
+      </CardActions>
+    </StyledCard>
+  );
+};
+
+export default ThemeCard;
